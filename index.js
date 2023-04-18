dc88ff2d1ebbdfc64d8503a93c3d792f52360406
Julio Radilla <itsdigitaljulio@gmail.com>
2023-04-18 18:50:04 -0500

    Daily

---
+++ index.js
@@ -0,0 +1,17 @@
+/*DROPDOWN CONTENT */
+let dropdownContent = document.querySelector(".dropdown-content");
+let bars = document.querySelector(".fa-bars");
+let xmark = document.querySelector('.fa-xmark');
+
+bars.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+    xmark.classList.toggle('show')
+    bars.classList.toggle("none");
+})
+
+xmark.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+    bars.classList.toggle("none");
+    xmark.classList.toggle("show");
+})
+/*DROPDOWN CONTENT */
